16e53a9359af406eaf65553d38fe97bfcac769d5
elvincelilov <celilovelvin11@gmail.com>
2026-05-05 10:31:19 +0200

    Refactor application with clean architecture, JPA integration, validation, testing, and Docker support

--- src/test/java/com/sap/refactoring/UserRepositoryTest.java
+++ src/test/java/com/sap/refactoring/UserRepositoryTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-@Transactional
 public class UserRepositoryTest {
 
     @Autowired
